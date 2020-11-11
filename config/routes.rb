461c2d1d27f1e7bd341fe795bea287ca3851a294
catsinspacesuits <simunro@hotmail.co.uk>
2020-11-11 20:47:41 +0100

    Amend categorised samples route url

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/tagged', to: 'samples#tagged', as: :tagged
   get '/tags', to: 'samples#tags_index'
   resources :categories
-  get '/categorised_samples', to: 'categories#categorise_samples'
+  get '/categorised', to: 'categories#categorise_samples'
   resources :libraries
 end
